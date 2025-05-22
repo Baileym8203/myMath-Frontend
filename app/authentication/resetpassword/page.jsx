4346e40194f74ca5de026475245144a8a9cde61d
Bailey Mashore <bailey@LAPTOP-AQND99TU.>
2025-05-22 15:38:19 -0700

    changing client vs server component issues

--- app/authentication/resetpassword/page.jsx
+++ app/authentication/resetpassword/page.jsx
@@ -1,11 +1,9 @@
-"use client";
+
 import ResetPasswordComponent from './components/resetpw';
 
+export const dynamic = "force-dynamic";
 // will handle the user being able to reset their passwords
 export default function ResetPW() {
-return (
-<main>
-<ResetPasswordComponent />
-</main>
-)
+return <ResetPasswordComponent />;
+
 }
